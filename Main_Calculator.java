ea81803095bbf5bff2185bba19993a9c1d2c2cd4
Sonakshi Priya <50029165+sonakshi-priya@users.noreply.github.com>
2021-09-28 14:46:30 +0530

    Add files via upload

---
+++ Main_Calculator.java
@@ -0,0 +1,12 @@
+
+
+package calculator;
+
+public class Main_Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculator calc = new Calculator();
+	}
+
+}
